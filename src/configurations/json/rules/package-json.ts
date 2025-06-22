1aee227bc51b69f28641b9dbbe25e23f138aca7d
MorevM <max.seainside@gmail.com>
2025-06-22 20:39:07 +0300

    feat(package-json): Enforce package.json `bin` files to be in kebab case

--- src/configurations/json/rules/package-json.ts
+++ src/configurations/json/rules/package-json.ts
@@ -181,7 +181,9 @@ export default defineConfigurationPart({
 
 		// Enforce that the bin property is valid
 		// https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/blob/main/docs/rules/valid-bin.md
-		'package-json/valid-bin': 'error',
+		'package-json/valid-bin': ['error', {
+			enforceCase: true,
+		}],
 
 		// Enforce that package names are valid npm package names
 		// https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/blob/main/docs/rules/valid-name.md
