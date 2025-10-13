bc05894c8d4f5aaa265138268c5133906a866368
MorevM <max.seainside@gmail.com>
2025-10-13 18:26:54 +0300

    chore(html): Update rule links to actual

--- src/configurations/html/rules/accessibility.ts
+++ src/configurations/html/rules/accessibility.ts
@@ -7,15 +7,15 @@ export default defineConfigurationPart({
 	},
 	rules: {
 		// Disallow use of abstract roles
-		// https://yeonjuan.github.io/html-eslint/docs/rules/no-abstract-roles
+		// https://html-eslint.org/docs/rules/no-abstract-roles
 		'@html-eslint/no-abstract-roles': 'error',
 
 		// Disallow accesskey attributes
-		// https://yeonjuan.github.io/html-eslint/docs/rules/no-accesskey-attrs
+		// https://html-eslint.org/docs/rules/no-accesskey-attrs
 		'@html-eslint/no-accesskey-attrs': 'error',
 
 		// Disallow to use `aria-hidden` attributes on the `body` element
-		// https://yeonjuan.github.io/html-eslint/docs/rules/no-aria-hidden-body
+		// https://html-eslint.org/docs/rules/no-aria-hidden-body
 		'@html-eslint/no-aria-hidden-body': 'error',
 
 		// Disallow the use of `aria-hidden="true"` on focusable elements
@@ -35,15 +35,15 @@ export default defineConfigurationPart({
 		'@html-eslint/no-invalid-role': 'error',
 
 		// Disallow use of `user-scalable=no` in `<meta name="viewport">`
-		// https://yeonjuan.github.io/html-eslint/docs/rules/no-non-scalable-viewport
+		// https://html-eslint.org/docs/rules/no-non-scalable-viewport
 		'@html-eslint/no-non-scalable-viewport': 'error',
 
 		// Disallow use of positive `tabindex` attribute
-		// https://yeonjuan.github.io/html-eslint/docs/rules/no-positive-tabindex
+		// https://html-eslint.org/docs/rules/no-positive-tabindex
 		'@html-eslint/no-positive-tabindex': 'error',
 
 		// Disallow skipping heading levels
-		// https://yeonjuan.github.io/html-eslint/docs/rules/no-skip-heading-levels
+		// https://html-eslint.org/docs/rules/no-skip-heading-levels
 		'@html-eslint/no-skip-heading-levels': 'warn',
 
 		// Enforces to use a valid `method` attribute on the `<form>`
@@ -51,11 +51,11 @@ export default defineConfigurationPart({
 		'@html-eslint/require-form-method': 'error',
 
 		// Require title attribute in `<frame>` and `<iframe>`
-		// https://yeonjuan.github.io/html-eslint/docs/rules/require-frame-title
+		// https://html-eslint.org/docs/rules/require-frame-title
 		'@html-eslint/require-frame-title': 'error',
 
 		// Require `alt` attribute at `img` tag
-		// https://yeonjuan.github.io/html-eslint/docs/rules/require-img-alt
+		// https://html-eslint.org/docs/rules/require-img-alt
 		'@html-eslint/require-img-alt': 'error',
 
 		// Enforces the presence of accessible labels for input elements
@@ -63,7 +63,7 @@ export default defineConfigurationPart({
 		'@html-eslint/require-input-label': 'error',
 
 		// Enforce to use `<meta name="viewport" ...>` in the `<head></head>`
-		// https://yeonjuan.github.io/html-eslint/docs/rules/require-meta-viewport
+		// https://html-eslint.org/docs/rules/require-meta-viewport
 		'@html-eslint/require-meta-viewport': 'error',
 	},
 });
