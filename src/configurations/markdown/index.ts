c7a3bec1c6ba6b2eab536e8368a5712246901a19
MorevM <max.seainside@gmail.com>
2025-10-18 19:42:55 +0300

    chore: Rename markdownlint file

--- src/configurations/markdown/index.ts
+++ src/configurations/markdown/index.ts
@@ -4,7 +4,7 @@ import { parserMarkdown } from '#parsers';
 import { defineConfigurationPart, mergeParts } from '#utils';
 import { universalRules } from '~configurations/universal-rules';
 
-import markdown from './rules/markdown';
+import markdownlint from './rules/markdownlint';
 
 import type { MarkdownConfigurationOptions } from '#types';
 
@@ -30,7 +30,7 @@ export default function configurationMarkdown(options: Partial<MarkdownConfigura
 			files,
 			ignores,
 			...mergeParts(
-				markdown,
+				markdownlint,
 			),
 		}),
 		defineConfigurationPart({
