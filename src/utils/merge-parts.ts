6fb2d8c38334feac67a72cebd694986b06123afe
MorevM <max.seainside@gmail.com>
2025-10-19 15:36:34 +0300

    fix: Allow `language` as a part of `mergeParts` utility

--- src/utils/merge-parts.ts
+++ src/utils/merge-parts.ts
@@ -1,5 +1,12 @@
-import type { NonEmptyArray } from '@morev/utils';
+import { arrayUnique  } from '@morev/utils';
+import type { Explicit, NonEmptyArray } from '@morev/utils';
 import type { FlatConfig } from '#types';
+
+type Part = Explicit<{
+	plugins: FlatConfig['plugins'];
+	rules: FlatConfig['rules'];
+	language?: FlatConfig['language'];
+}>;
 
 /**
  * Utility function to merge configuration parts defined in the `configurations` folder.
@@ -9,8 +16,22 @@ import type { FlatConfig } from '#types';
  * @returns           Type-safe configuration parts merged into a single object.
  */
 export const mergeParts = (...objects: NonEmptyArray<FlatConfig>) => {
-	return {
+	const uniqueKeys = arrayUnique(objects.flatMap((object) => Object.keys(object)));
+
+	const part: Part = {
 		plugins: objects.reduce<FlatConfig['plugins']>((acc, config) => ({ ...acc, ...config.plugins }), {}),
 		rules: objects.reduce<FlatConfig['rules']>((acc, config) => ({ ...acc, ...config.rules }), {}),
 	};
+
+	if (uniqueKeys.includes('language')) {
+		const language = objects.reduce<FlatConfig['language']>((acc, config) => {
+			return config.language ?? acc;
+		}, undefined);
+
+		if (language) {
+			part.language = language;
+		}
+	}
+
+	return part;
 };
