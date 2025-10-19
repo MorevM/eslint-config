17e95021e4d42b894df18a4c74bd7375d8d7e379
MorevM <max.seainside@gmail.com>
2025-10-19 16:24:40 +0300

    refactor(markdown): Replace `markdownlint/md034` with `markdown/no-bar-urls`, decrease severity to `warn`

--- src/configurations/markdown/rules/markdown.ts
+++ src/configurations/markdown/rules/markdown.ts
@@ -14,5 +14,9 @@ export default defineConfigurationPart({
 		// Require languages for fenced code blocks
 		// https://github.com/eslint/markdown/blob/main/docs/rules/heading-increment.md
 		'markdown/heading-increment': 'warn',
+
+		// Disallow bare URLs (autofixable)
+		// https://github.com/eslint/markdown/blob/main/docs/rules/no-bare-urls.md
+		'markdown/no-bare-urls': 'error',
 	},
 });
