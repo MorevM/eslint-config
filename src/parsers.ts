54d79b574641cee01b19ef3ad2b7cf9a3ab64979
MorevM <max.seainside@gmail.com>
2026-02-07 23:13:35 +0300

    chore: Bump dependencies that do not require additional setup

--- src/parsers.ts
+++ src/parsers.ts
@@ -3,4 +3,4 @@ export { default as parserAstro } from 'astro-eslint-parser';
 export { default as parserJson } from 'jsonc-eslint-parser';
 export { parser as parserTypescript } from 'typescript-eslint';
 export { default as parserVue } from 'vue-eslint-parser';
-export { default as parserYaml } from 'yaml-eslint-parser';
+export * as parserYaml from 'yaml-eslint-parser';
