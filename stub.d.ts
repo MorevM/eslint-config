fd265598d85dcc307ed2475d18878b8e6179d023
MorevM <max.seainside@gmail.com>
2026-02-11 00:53:25 +0300

    refactor(javascript): Switch to a mainainable fork of `eslint-comments` plugin

--- stub.d.ts
+++ stub.d.ts
@@ -1,11 +1,4 @@
 /* eslint-disable no-duplicate-imports -- False positives with `declare module` */
-declare module 'eslint-plugin-eslint-comments' {
-	import type { ESLint } from 'eslint';
-
-	declare const _default: ESLint.Plugin;
-	export default _default;
-}
-
 declare module 'eslint-plugin-no-secrets' {
 	import type { ESLint } from 'eslint';
 
