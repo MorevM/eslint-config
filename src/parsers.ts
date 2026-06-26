1d22773febecad5243ba4e1f82ba380447ffc41e
MorevM <max.seainside@gmail.com>
2026-06-26 14:25:07 +0300

    chore!: Bump `eslint-plugin-astro` from 1.5.0 to 2.1.1
    BREAKING CHANGE: Per updated `eslint-plugin-astro` constraints minimal required Node version now is 22.22.3

--- src/parsers.ts
+++ src/parsers.ts
@@ -1,5 +1,5 @@
 export { default as parserHtml } from '@html-eslint/parser';
-export { default as parserAstro } from 'astro-eslint-parser';
+export * as parserAstro from 'astro-eslint-parser';
 export { default as parserJson } from 'jsonc-eslint-parser';
 export { parser as parserTypescript } from 'typescript-eslint';
 export { default as parserVue } from 'vue-eslint-parser';
