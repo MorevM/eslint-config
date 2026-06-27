4a2bf40b7376d96fbe38d2b54e074f1abe071862
MorevM <max.seainside@gmail.com>
2026-06-27 13:06:25 +0300

    chore: Bump `jsonc-eslint-parser` from 2.4.2 to 3.1.0

--- src/parsers.ts
+++ src/parsers.ts
@@ -1,6 +1,6 @@
 export { default as parserHtml } from '@html-eslint/parser';
 export * as parserAstro from 'astro-eslint-parser';
-export { default as parserJson } from 'jsonc-eslint-parser';
+export * as parserJson from 'jsonc-eslint-parser';
 export { parser as parserTypescript } from 'typescript-eslint';
 export { default as parserVue } from 'vue-eslint-parser';
 export * as parserYaml from 'yaml-eslint-parser';
